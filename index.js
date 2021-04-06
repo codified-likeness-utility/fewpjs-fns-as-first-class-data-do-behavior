6c9bee0bc3e07a5c505201a0cea45c0782c67fd8
Ian Davies <mr.daviesian@gmail.com>
2021-04-06 17:39:34 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  let newTime = time.replace(/\D/g,'')
+
+  if (newTime < 1200 ) {
+    return "Good Morning"
+  }
+  if (newTime > 1200 && newTime < 1700) {
+    return "Good Afternoon"
+  }
+  if (newTime > 1700) {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  let newGreeting = document.getElementById('greeting')
+  newGreeting.innerHTML = `${string}`
+}
